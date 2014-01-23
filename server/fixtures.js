5f32f7ddc1e528a96410a6ebd9448d7a938bc0a5
Milo Thurston <knirirr@gmail.com>
2014-01-23 05:38:04 -0500

    Added more files

---
+++ server/fixtures.js
@@ -0,0 +1,8 @@
+if (Questions.find().count() === 0) {
+  var q1 = Questions.insert({
+    title:  "How interesting is this project?"
+  });
+var q1 = Questions.insert({
+    title:  "Seriously, is it interesting?"
+  });
+}
